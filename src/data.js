3bd3c8efbc8983cf93cd448f4ff7ea901f110125
vishnukannanklk <vishnukannan1999klk@gmail.com>
2025-05-17 21:20:35 +0530

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,73 @@
+
+ export const data = [
+    {
+        Question: 'What is the main purpose of React?',
+        Option1: 'To perform mathematical calculations',
+        Option2: 'To style web pages',
+        Option3: 'To build user interfaces',
+        Option4: 'To manage databases'
+    },
+    {
+        Question: 'Which method is used to create a React component using a function?',
+        Option1: 'React.createComponent()',
+        Option2: 'function Component()',
+        Option3: 'React.useComponent()',
+        Option4: 'React.defineComponent()'
+    },
+    {
+        Question: 'What is Redux?',
+        Option1: 'A JavaScript library for styling components',
+        Option2: 'A database for storing React files',
+        Option3: 'A state management library for JavaScript applications',
+        Option4: 'A testing framework for React apps'
+    },
+    {
+        Question: 'What does JSX stand for?',
+        Option1: 'JavaScript Extended',
+        Option2: 'Java Syntax Extension',
+        Option3: 'JavaScript XML',
+        Option4: 'JSON XML'
+    },
+    {
+         Question: 'What hook is used to manage state in a functional component?',
+        Option1: 'useData()',
+        Option2: 'useState()',
+        Option3: 'setState()',
+        Option4: 'state()'
+    },
+    {
+        Question: 'Which of these is not a valid React lifecycle method?',
+        Option1: 'componentDidMount()',
+        Option2: 'componentWillUnmount()',
+        Option3: 'componentWillRender()',
+        Option4: 'shouldComponentUpdate()'
+    },
+    {
+        Question: 'How can you pass data from a parent component to a child component?',
+        Option1: 'Using props',
+        Option2: 'Using state',
+        Option3: 'Using hooks',
+        Option4: 'Using setData'
+    },
+    {
+        Question: 'What does the useEffect() hook do?',
+        Option1: 'Adds routing',
+        Option2: 'Handles events',
+        Option3: 'Performs side effects like API calls',
+        Option4: 'Creates state'
+    },
+    {
+        Question: 'In React, what is a “key” prop used for?',
+        Option1: 'Identifying unique CSS classes',
+        Option2: 'Setting access control',
+        Option3: 'Identifying elements in a list',
+        Option4: 'Passing secure data'
+    },
+    {
+         Question: 'What is the correct way to handle an event in React?',
+        Option1: '<button onClick={handleClick()>Click</button>',
+        Option2: '<button onclick={handleClick()>Click</button>',
+        Option3: '<button onClick={handleClick}>Click</button>',
+        Option4: '<button on-click={handleClick}>Click</button>'
+    }
+]
